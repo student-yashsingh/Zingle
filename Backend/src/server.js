9b6338dd4c0969f90aba3dfce3cbea61ec7efe71
student-yashsingh <yash1243singh@gmail.com>
2025-11-08 10:45:24 +0530

    final commits for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,57 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import path from "path";
-// import { fileURLToPath } from "url";
-// import authRoutes from "./Routes/auth.route.js";
-// import userRoutes from "./Routes/auth.route.js";
-// import chatRoutes from "./Routes/chat.route.js";
-// import { connectDB } from "./library/database.js";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// // Load .env from Backend folder (one level above src)
-// dotenv.config({ path: path.resolve(__dirname, "../.env") });
-
-// const app = express();
-// const PORT = process.env.PORT || 5001;
-
-// app.use(cors({
-//     origin:"http://localhost:5173",
-//     credentials:true   // allows the frontend to send the cookies
-// }));
-
-
-
-
-// app.get("/api/auth/signup",(req,res)=>{
-//     res.send("signup page");
-//     })
-// app.get("/api/auth/login",(req,res)=>{
-//     res.send("log in page");
-// })
-// app.get("/api/auth/signup",(req,res)=>{
-//     res.send("log out page");
-// })
-
-// app.use(express.json());
-// app.use(cookieParser());
-
-// app.use("/api/auth", authRoutes);
-// app.use("/api/users",userRoutes);
-// app.use("/api/chat",chatRoutes);
-
-// app.listen(PORT, () => {
-//   connectDB();
-//   console.log(` Server is running at http://localhost:${PORT}`);
-// });
-
-
-
-
-// Backend/src/server.js
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -72,7 +18,7 @@ import { connectDB } from "./lib/database.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
+ const ___dirname=path.resolve();
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -86,6 +32,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(___dirname,"../frontend/dist")));
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(___dirname,"../frontend","dist","index.html"));
+
+  })
+}
 
 app.get("/api/health", (_, res) => res.json({ ok: true }));
 
--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -10,7 +10,7 @@
 
 
 import axios from "axios";
-
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 const axiosInstance = axios.create({
   baseURL: "http://localhost:5001/api",
   withCredentials: true,
