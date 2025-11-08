b16871254a0a52af49eff91f90522530f0d2a16d
student-yashsingh <yash1243singh@gmail.com>
2025-11-08 14:30:09 +0530

    temp rename

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,7 +6,7 @@ import LoginPage from "./pages/LoginPage.jsx";
 import NotificationsPage from "./pages/NotificationsPage.jsx";
 import CallPage from "./pages/CallPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
-import OnboardingPage from "./pages/OnBoardingPage.jsx";
+import OnboardingPage from "./pages/temp.jsx";
 
 
 import FriendsPage from "./pages/FriendsPage.jsx";   
