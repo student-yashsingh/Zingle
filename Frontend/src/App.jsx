bf3fbe8794d9109239a4e59982e47d25d627f6f9
student-yashsingh <yash1243singh@gmail.com>
2025-11-08 14:37:50 +0530

    final changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,8 +6,8 @@ import LoginPage from "./pages/LoginPage.jsx";
 import NotificationsPage from "./pages/NotificationsPage.jsx";
 import CallPage from "./pages/CallPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
-import OnboardingPage from "./pages/OnboardingPage.jsx";
-import FriendsPage from "./pages/FriendsPage.jsx";   
+import FriendsPage from "../pages/FriendsPage.jsx";
+import OnboardingPage from "../pages/OnboardingPage.jsx";
 
 import { Toaster } from "react-hot-toast";
 
