40c3aa61ee7b0dd608ee490502d196f8910597ed
student-yashsingh <yash1243singh@gmail.com>
2025-12-01 19:34:11 +0530

    final done

--- Backend/src/Routes/auth.route.js
+++ Backend/src/Routes/auth.route.js
@@ -1,28 +1,3 @@
-// import express from "express"
-// import {signup,login,logout,onboard} from "../controllers/auth.controller.js"
-// import { protectRoute } from "../middleware/auth.middleware.js";
-// const router=express.Router();
-
-// router.post("/signup",signup);
-
-// router.post("/login",login);
-
-// router.post("/logout",logout);
-
-// router.post("/onboarding",protectRoute,onboard);
-
-
-
-// // check if the user is logged in 
-// router.get("/me", protectRoute, (req, res) => {
-//     res.status(200).json({ success: true, user: req.user });
-// });
-
-// export default router; 
-
-
-
-// Backend/src/Routes/auth.route.js
 import express from "express";
 import { login, logout, onboard, signup } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
